8cc7483e4da731eabb161f21428bb742112e0a9e
Alex Fazakas <alexcf0402@gmail.com>
2024-02-14 19:58:49 +0100

    final

--- src/models.py
+++ src/models.py
@@ -25,6 +25,31 @@ class Address(Base):
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
 
+class FavoriteItem(Base):
+    __tablename__ = 'favorite_items'
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True, nullable=False)
+    planet_id = Column(Integer, ForeignKey('planets.id'), nullable=True)
+    character_id = Column(Integer, ForeignKey('characters.id'), nullable=True)
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    favorites = relationship('FavoriteItem', back_populates='user')
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    diameter = Column(String(250), nullable=True)
+    name = Column(String(250), nullable=True)
+    population = Column(Integer)
+    favorite_id = relationship('FavoriteItem', backref='planets')
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    last_name = Column(String(250))
+    height = Column(Float)
+    favorite_id = relationship('FavoriteItem', backref='characters')     
+
     def to_dict(self):
         return {}
 
